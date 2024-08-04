e02f483c5efbe463f7702a26d22fd2acf973215f
Lauryn Thayne <laurynthayne@Daren-Thayne-J62060W5VX.local>
2024-08-03 21:33:45 -0700

    some further study complete

--- src/further-study.js
+++ src/further-study.js
@@ -6,14 +6,35 @@
 //   => ['a']
 //   commonStrings(['zoo', 'space', 'zoo'], ['zoo', 'space', 'boat']);
 //   => ['zoo', 'space']
-function commonStrings(array1, array2) {}
+function commonStrings(array1, array2) {
+    let newArray = [];
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
+              if (newArray.includes(array1[i]) === false) {
+                newArray.push(array1[i]);
+              }
+            }
+        } 
+    } 
+    return newArray;
+}
 
 // Given three numbers, return a list of numbers from 1 to 100 that are
 // divisible by at least one of a, b or c.
 // Ex.:
 //   divisibleByEither(50, 30, 29);
 //   => [29, 30, 50, 58, 60, 87, 90, 100]
-function divisibleByEither(a, b, c) {}
+function divisibleByEither(a, b, c) {
+    let newArray = [];
+    for (let i = 1; i <= 100; i++) {
+      if (i % a === 0 || i % b === 0 || i % c === 0) {
+        newArray.push(i);
+      }
+    }
+    return newArray;
+  } 
+  
 
 // Compress a string using the rules below and return the result. To compress a
 // string, replace consecutive duplicate characters with a number and the
